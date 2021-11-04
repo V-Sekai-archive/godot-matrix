2b9978ae654a73bb1fc5dd9c7005516dfbfd4ed7
Houkime <>
2021-11-04 15:31:08 +0000

    cleanup message_text

--- example/message_text.gd
+++ example/message_text.gd
@@ -1,12 +1,6 @@
 extends Label
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here    
 	connect("item_rect_changed", self, "on_item_rect_changed")
 	on_item_rect_changed()
 
